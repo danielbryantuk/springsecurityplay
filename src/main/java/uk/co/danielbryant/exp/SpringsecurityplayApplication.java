babbbfe842f45b91e54563021f8699532dac1471
Daniel Bryant <daniel.bryant@tai-dev.co.uk>
2017-02-08 09:44:18 +0000

    Initial commit

---
+++ src/main/java/uk/co/danielbryant/exp/SpringsecurityplayApplication.java
@@ -0,0 +1,12 @@
+package uk.co.danielbryant.exp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringsecurityplayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringsecurityplayApplication.class, args);
+	}
+}
